b5c3e9b635910507e58994deb35d9d782e674c5d
Benjamin Mayrargue <benjamin@vapolia.fr>
2019-02-20 11:22:42 +0100

    upgrade package reference. upgrade build. add uwp. upgrade nuget.

--- nuget/build.bat
+++ nuget/build.bat
@@ -1,12 +1,12 @@
-set version=200.0.6
+set version=200.0.7
 
 
 mkdir SQLite.Net
-copy /y ..\src\SQLite.Net\bin\Release\SQLite.Net.dll SQLite.Net
+copy /y ..\src\SQLite.Net\bin\Release\netstandard1.1\SQLite.Net.dll SQLite.Net
 type nul >SQLite.Net\_._
 
-mkdir SQLite.Net.Async
-copy /y ..\src\SQLite.Net.Async\bin\Release\SQLite.Net.Async.dll SQLite.Net.Async
+rem mkdir SQLite.Net.Async
+rem copy /y ..\src\SQLite.Net.Async\bin\Release\SQLite.Net.Async.dll SQLite.Net.Async
 
 mkdir SQLite.Net.Platform.Generic
 copy /y ..\src\SQLite.Net.Platform.Generic\bin\Release\SQLite.Net.Platform.Generic.dll SQLite.Net.Platform.Generic
@@ -17,27 +17,20 @@ copy /y ..\src\SQLite.Net.Platform.Win32\bin\Release\SQLite.Net.Platform.Win32.d
 mkdir SQLite.Net.Platform.NetCore
 copy /y ..\src\SQLite.Net.Platform.NetCore\bin\Release\netcoreapp2.0\SQLite.Net.Platform.NetCore.dll SQLite.Net.Platform.NetCore
 
-mkdir SQLite.Net.Platform.WindowsPhone8
-mkdir SQLite.Net.Platform.WindowsPhone8\ARM
-mkdir SQLite.Net.Platform.WindowsPhone8\x86
-copy /y ..\src\SQLite.Net.Platform.WindowsPhone8\bin\x86\Release\SQLite.Net.Platform.WindowsPhone8.dll SQLite.Net.Platform.WindowsPhone8\x86
-copy /y ..\src\SQLite.Net.Platform.WindowsPhone8\bin\ARM\Release\SQLite.Net.Platform.WindowsPhone8.dll SQLite.Net.Platform.WindowsPhone8\ARM
-
-mkdir SQLite.Net.Platform.WinRT
-copy /y ..\src\SQLite.Net.Platform.WinRT\bin\Release\SQLite.Net.Platform.WinRT.dll SQLite.Net.Platform.WinRT
-
 mkdir SQLite.Net.Platform.XamarinAndroid
 copy /y ..\src\SQLite.Net.Platform.XamarinAndroid\bin\Release\SQLite.Net.Platform.XamarinAndroid.dll SQLite.Net.Platform.XamarinAndroid
 
 mkdir SQLite.Net.Platform.XamarinIOS.Unified
 copy /y ..\src\SQLite.Net.Platform.XamarinIOS.Unified\bin\Release\SQLite.Net.Platform.XamarinIOS.Unified.dll SQLite.Net.Platform.XamarinIOS.Unified
 
+mkdir SQLite.Net.Platform.Uwp
+copy /y ..\src\SQLite.Net.Platform.Uwp\bin\Release\SQLite.Net.Platform.Uwp.dll SQLite.Net.Platform.Uwp
 
 
 @mkdir output
 del /q	output\*.*
-nuget pack SQLite.Net.nuspec -o output -Version %version%
-nuget pack SQLite.Net.Async.nuspec -o output -Version %version%
+nuget pack SQLite.Net.nuspec -OutputDirectory output -Version %version%
+rem nuget pack SQLite.Net.Async.nuspec -OutputDirectory output -Version %version%
 
 
 nuget push output\*.nupkg -Source http://nugets.vapolia.fr/
---
+++ src/SQLite.Net.Platform.Uwp/SQLitePlatform.cs
@@ -0,0 +1,20 @@
+﻿using SQLite.Net.Interop;
+
+namespace SQLite.Net.Platform.NetCore
+{
+    public class SQLitePlatform : ISQLitePlatform
+    {
+        public SQLitePlatform()
+        {
+            SQLiteApi = new SQLiteApi();
+            StopwatchFactory = new StopwatchFactory();
+            ReflectionService = new ReflectionService();
+            VolatileService = new VolatileService();
+        }
+
+        public ISQLiteApi SQLiteApi { get; private set; }
+        public IStopwatchFactory StopwatchFactory { get; private set; }
+        public IReflectionService ReflectionService { get; private set; }
+        public IVolatileService VolatileService { get; private set; }
+    }
+}
