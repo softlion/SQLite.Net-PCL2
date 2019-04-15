610d2cf1e6974f7e4e31e5dd83b521828904ff3c
Benjamin Mayrargue <benjamin@vapolia.fr>
2019-04-15 14:29:29 +0200

    nuget

---
+++ nuget/pack.ps1
@@ -0,0 +1,26 @@
+######NuGet.CommandLine is too old and does not support Xamarin.iOS libs
+#####Please install the package "NuGe t.CommandLine" from https://chocolatey.org/ before running this script
+#####After chocolatey is installed, type: choco install NuGet.CommandLine
+#####Before running this script, download nuget.exe from @echo https://nuget.codeplex.com/releases/view/133091
+#####and put nuget.exe in the path.
+
+#####set /p nugetServer=Enter base nuget server url (with /): 
+$nugetServer="http://nugets.vapolia.fr/"
+
+#####################
+#Build release config
+cd ..
+$msbuild = 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe'
+nuget restore
+$msbuildparams = '/t:Clean;Build', '/p:Configuration=Release', '/p:Platform=Any CPU', 'SQLite.Net.sln'
+& $msbuild $msbuildparams
+cd nuget
+
+del *.nupkg
+
+$version="204.0.0-pre1"
+nuget pack "SQLite.Net.nuspec" -Version $version
+nuget push "SQLite.Net-PCL.$version.nupkg" -Source $nugetServer
+
+#####set assembly info to version
+#####https://gist.github.com/derekgates/4678882
