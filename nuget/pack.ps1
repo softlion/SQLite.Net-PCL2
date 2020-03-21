ff5685fe828b15cacba9fd6f0ad8d4f74903be41
Benjamin Mayrargue <benjamin@vapolia.fr>
2020-03-21 14:34:47 +0100

    upgrade/simplify nuget generation

--- nuget/pack.ps1
+++ nuget/pack.ps1
@@ -1,26 +1,19 @@
-######NuGet.CommandLine is too old and does not support Xamarin.iOS libs
-#####Please install the package "NuGe t.CommandLine" from https://chocolatey.org/ before running this script
-#####After chocolatey is installed, type: choco install NuGet.CommandLine
-#####Before running this script, download nuget.exe from @echo https://nuget.codeplex.com/releases/view/133091
-#####and put nuget.exe in the path.
-
-#####set /p nugetServer=Enter base nuget server url (with /): 
 $nugetServer="https://www.nuget.org"
+if ($IsMacOS) {
+    $msbuild = "msbuild"
+} else {
+    $vswhere = 'C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe'
+    $msbuild = & $vswhere -latest -products * -requires Microsoft.Component.MSBuild -property installationPath
+    $msbuild = join-path $msbuild 'MSBuild\Current\Bin\MSBuild.exe'
+}
+$version="2.0.2"
+$versionSuffix=""
 
 #####################
 #Build release config
-cd ..
-$msbuild = 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe'
-nuget restore
-$msbuildparams = '/t:Clean;Build', '/p:Configuration=Release', '/p:Platform=Any CPU', 'SQLite.Net.sln'
-& $msbuild $msbuildparams
-cd nuget
-
+cd $PSScriptRoot
 del *.nupkg
+& $msbuild "../SQLite.Net.sln" /restore /p:Configuration=Release /p:Platform="Any CPU" /p:Version="$version" /p:VersionSuffix="$versionSuffix" /p:Deterministic=false /p:PackageOutputPath="$PSScriptRoot" --% /t:Clean;Build;Pack
+if ($lastexitcode -ne 0) { exit $lastexitcode; }
 
-$version="2.0.2"
-nuget pack "sqlite-net2.nuspec" -Version $version
 nuget push "sqlite-net2.$version.nupkg" -Source $nugetServer
-
-#####set assembly info to version
-#####https://gist.github.com/derekgates/4678882
