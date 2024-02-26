0dc2a4f395aabed3472d5f77482c30f5fb9d3676
Benjamin Mayrargue <benjamin@vapolia.fr>
2024-02-26 16:08:10 +0100

    2.1.0 net8

--- nuget/pack.ps1
+++ nuget/pack.ps1
@@ -1,20 +1,12 @@
-$nugetServer="https://www.nuget.org"
-if ($IsMacOS) {
-    $msbuild = "msbuild"
-} else {
-    $vswhere = 'C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe'
-    $msbuild = & $vswhere -latest -products * -requires Microsoft.Component.MSBuild -property installationPath
-    $msbuild = join-path $msbuild 'MSBuild\Current\Bin\MSBuild.exe'
-}
-$version="2.1.0"
 $versionSuffix=""
 
 #####################
 #Build release config
 cd $PSScriptRoot
 del *.nupkg
-& $msbuild "../SQLite.Net.sln" /restore /p:Configuration=Release /p:Platform="Any CPU" /p:Version="$version" /p:VersionSuffix="$versionSuffix" /p:Deterministic=false /p:PackageOutputPath="$PSScriptRoot" --% /t:Clean;Build;Pack
+
+& dotnet pack -c Release --version-suffix "$versionSuffix" ../src/SQLite.Net/SQLite.Net2.csproj -o .
+
 if ($lastexitcode -ne 0) { exit $lastexitcode; }
 
-nuget push "sqlite-net2.$version$versionSuffix.nupkg" -Source $nugetServer
-#copy "sqlite-net2.$version$versionSuffix.nupkg" "D:\repos\localnugets"
+dotnet nuget push "sqlite-net2.*$versionSuffix.nupkg"
